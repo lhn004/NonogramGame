6e8e53fbe284ea731fdb2314416b2b74334dd596
Linh Nguyen <lhn004@bucknell.edu>
2023-01-24 21:07:43 -0500

    lab01, Exercise 1,Complete

---
+++ src/main/java/lab01/Hello.java
@@ -0,0 +1,22 @@
+/* *****************************************
+*CSCI205 -Software Engineering and Design
+* Spring2023
+* Instructor: Brian King
+* Section: 9am
+*
+* Name: Linh Nguyen
+* Date: 01/24/2023
+*
+*Lab / Assignment: Lab 01 - Hello, Java!
+*
+*Description: A "HelloWorld"program in Java
+* * *****************************************/
+
+package lab01;
+public class Hello
+{
+    public static void main(String[] args){
+        System.out.println("Programming is not a spectator sport!");
+        System.exit(0);
+    }
+}
